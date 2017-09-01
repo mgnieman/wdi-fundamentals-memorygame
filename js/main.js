e99d4d625d04882804822dd003f18b4019754466
Michael Nieman <michael.nieman@libertymutual.com>
2017-09-01 11:19:48 -0400

    created game logic

--- js/main.js
+++ js/main.js
@@ -9,15 +9,20 @@ var cardTwo = cards[1];
 var cardThree = cards[2];
 var cardFour = cards[3];
 
-cardsInPlay.push(cardTwo, cardThree);
-
-console.log("User flipped " + cardsInPlay[0]);
-console.log("User flipped " + cardsInPlay[1]);
-
-if (cardsInPlay.length === 2) {
+var checkForMatch = function() {
 	if (cardsInPlay[0] === cardsInPlay[1]) {
 		alert("You found a match!");
 	} else {
 		alert("Sorry, try again.");
 	}
 }
+
+var flipCard = function(cardID) {
+	console.log("User flipped " + cards[cardID]);
+	cardsInPlay.push(cards[cardID]);
+	if (cardsInPlay.length === 2) {
+		checkForMatch();
+	}
+}
+flipCard(1);
+flipCard(3);
